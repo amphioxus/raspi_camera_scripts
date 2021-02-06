6efc1b4d3dbd259faa54d9740db245bfea50f93b
amphioxus <armin.hinterwirth@icloud.com>
2021-02-05 21:48:50 -0800

    Stop button needs to be held 2 seconds, then progam exits. (Needed signal to do so.)

--- camera_button_trigger_simple.py
+++ camera_button_trigger_simple.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import picamera
 from gpiozero import LED, Button
 from datetime import datetime
-from signal import pause
+import signal
 import os, sys
 
 """
@@ -30,28 +32,37 @@ SCREEN_DIMS = (1352, 640) #  # Dimension of live preview, e.g. (1920,1080)
 IMG_LOCATION = '/home/pi/Pictures'
 # Set up which pins to use for LED and buttons
 led = LED(17)
-button = Button(18) # trigger button
-button2 = Button(24) # exit button
+button = Button(23) # trigger button
+button2 = Button(25, hold_time=2) # exit button
 
+# workaround for using a button to kill the signal.pause()
+# https://www.raspberrypi.org/forums/viewtopic.php?t=268903
+# Handler for SIGUSER1
+def handleSignal(num, stack):
+  #print('Got signal! And now we mysteriously will exit....')
+  pass # we don't really need to do anything with this, just avoid error mess.
+  
+signal.signal(signal.SIGUSR1, handleSignal)
 
 def take_picture():
     """
     Gets called when button is pressed
     """
+    print('Taking a picture!')
     timestamp=datetime.now().isoformat()
     img_name = 'image_'+str(timestamp)+'.jpg'
     img_path = os.path.join(IMG_LOCATION, img_name)
     
     led.on        
     camera.capture(img_path)
-    led.off
-    
+    led.off    
     print('Image saved as : {}'.format(img_path))
 
 def stop_program():
     """Close the program to get rid of video overlay"""
     print('Stop button pressed')
-    sys.exit(0)
+    # sys.exit(0)
+    os.kill(os.getpid(), signal.SIGUSR1)
     
 # set up the camera        
 camera = picamera.PiCamera()
@@ -64,6 +75,7 @@ else:
 camera.start_preview(resolution=SCREEN_DIMS, fullscreen=True)
 
 button.when_pressed = take_picture
-button2.when_pressed = stop_progam
 
-pause()
+button2.when_held = stop_program
+
+signal.pause()
